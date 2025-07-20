2be0dbe3a282f435eee8b7f8195997aa63b3689f
Ariessa <bluepear304@gmail.com>
2025-07-20 20:12:34 +0800

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyALFWv24fYB0jjtv3cn5L56WMHKGYdJIuQ",
+    authDomain: "foodtrucktrack-c225a.firebaseapp.com",
+    databaseURL: "https://foodtrucktrack-c225a-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "foodtrucktrack-c225a",
+    storageBucket: "foodtrucktrack-c225a.appspot.com",
+    messagingSenderId: "872794794811",
+    appId: "1:872794794811:web:10f5d5c05907d024e1e51e"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
